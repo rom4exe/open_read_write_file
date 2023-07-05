8c059d4e30e10a2d00fecd69f0c30a18f753bde5
neto Safronov <rom.exe@mail.ru>
2023-07-05 14:11:56 +0800

    Task3_Polymorphism_and_magic_methods

--- main.py
+++ main.py
@@ -17,6 +17,21 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __str__(self) -> str:
+        if self.finished_courses:
+            fin = ", ".join(self.finished_courses)
+        else:
+            fin = "отсутствуют"
+        return f'имя: {self.name}\nфамилия: {self.surname}\n\
+Средняя оценка за домашние задания: {Averaged(self)}\nКурсы в процессе изучения: \
+{", ".join(self.courses_in_progress)}\nЗавершенные курсы: {fin}\n'
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print(f'сравнение возможно только одной грыппы: или преподователей или студентов')
+            return
+        return Averaged(self) < Averaged(other)
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -30,6 +45,15 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __str__(self) -> str:
+        return f'имя: {self.name}\nфамилия: {self.surname}\nСредняя оценка за лекции: {Averaged(self)}\n'
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print(f'сравнение возможно только одной грыппы: или преподователей или студентов')
+            return
+        return Averaged(self) < Averaged(other)
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -41,14 +65,30 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self) -> str:
+        return f'имя: {self.name}\nфамилия: {self.surname}\n'
+
+
+def Averaged(self):
+    a = 0
+    b = 0
+    if self.grades:
+        for i in self.grades.values():
+            for j in i:
+                a += 1
+                b += j
+        return b / a
+    else:
+        return "Оценок нет"
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
